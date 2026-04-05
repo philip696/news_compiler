9b4a5c4febb8767d3469dde62735bb342e607c2a
Philip Dewanto <philip,dewanto@gmail.com>
2026-04-05 20:04:16 +0800

    fix: use NEXT_PUBLIC_API_URL env variable

--- frontend/services/api.ts
+++ frontend/services/api.ts
@@ -1,6 +1,6 @@
 import axios from "axios";
 
-export const BASE_URL = process.env.NEXT_PUBLIC_API_BASE_URL || "http://127.0.0.1:8000";
+export const BASE_URL = process.env.NEXT_PUBLIC_API_URL || "http://127.0.0.1:8000";
 
 export const api = axios.create({
   baseURL: BASE_URL
